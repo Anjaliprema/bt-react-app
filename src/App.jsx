ab6e7a6aeeb403e3bf288d0d60c09734c2b03b7f
Anjaliprema <anjaliprema4321@gmail.com>
2026-02-26 13:27:36 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { ParallaxProvider } from 'react-scroll-parallax';
 import React from 'react';
 import Home_Static from './Components/Home_Static';
-// import Component from './Learn/Component';
 
 function App() {
   return (
     <ParallaxProvider>
       <Home_Static/>
-            {/* <Component />
-                  <div style={{ height: "100vh" }}></div> */}
     </ParallaxProvider>
   );
 }
