a1675bd16b09a356450d1e0570a8604ff9060007
Anjaliprema <anjaliprema4321@gmail.com>
2026-02-26 12:27:29 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { ParallaxProvider } from 'react-scroll-parallax';
 import React from 'react';
 import Home_Static from './Components/Home_Static';
-import Component from './Learn/Component';
+// import Component from './Learn/Component';
 
 function App() {
   return (
